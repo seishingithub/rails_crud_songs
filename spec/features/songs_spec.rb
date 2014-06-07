02fba3ad334c7bfc2a4104750d44caf40306c1b1
Peggy Griffin <seishingithub@gmail.com>
2014-06-07 13:59:38 -0600

    User can delete a song

--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -32,4 +32,20 @@ feature 'User can manage Songs' do
     expect(page).to have_no_content 'Open Arms'
     expect(page).to have_no_content 'Journey'
   end
+
+  scenario 'User can delete a song' do
+    visit '/'
+    click_on 'Add a song'
+    fill_in 'Song name', with: 'Open Arms'
+    fill_in 'Band name', with: 'Journey'
+    click_on 'Add song'
+    expect(page).to have_content 'Open Arms'
+    expect(page).to have_content 'Journey'
+    click_on 'Open Arms'
+    expect(page).to have_content 'Open Arms'
+    expect(page).to have_content 'Journey'
+    click_on 'Delete song'
+    expect(page).to have_no_content 'Open Arms'
+    expect(page).to have_no_content 'Journey'
+  end
 end
